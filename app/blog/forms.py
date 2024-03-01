7498d15e7e6105ed7aa4638e1eb4430b667353a7
Andrey <andreeey.gg@gmail.com>
2024-03-01 14:45:43 +0100

    replace login and register to another app

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -1,21 +1,29 @@
 from django import forms
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from captcha.fields import CaptchaField, CaptchaTextInput
+from .models import Review
+from django.core.exceptions import ValidationError
 
 
-class RegistrationUserForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    email = forms.EmailField(widget=forms.EmailField(attrs={'placeholder': 'Email'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repeat Password'}))
-    captcha = CaptchaField(widget=CaptchaTextInput(attrs={'placeholder': 'CAPTCHA'}))
-
+class AddReviewForm(forms.ModelForm):
     class Meta:
-        model = get_user_model()
-        fields = ['username', 'email', 'password1', 'password2']
+        model = Review
+        fields = ['title', 'text', 'images', 'category', 'is_published']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Title'}),
+            'text': forms.Textarea(attrs={'placeholder': 'Text'}),
+            'images': forms.FileInput(attrs={'placeholder': ''}),
+            'category': forms.Select(attrs={'class': ''}),
+            'is_published': forms.CheckboxInput(attrs={'class': ''}),
+        }
 
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 50:
+            raise ValidationError("Your title can't be longer than 50 symbols")
+        return title
 
-class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        if len(text) < 20:
+            raise ValidationError("Your text can't be less than 20 symbols")
+        return text
+
