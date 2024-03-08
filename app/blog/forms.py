cfadc8dbde9e900a9dd575a07ce3b8c4145e9b43
Andrey <andreeey.gg@gmail.com>
2024-03-08 15:00:14 +0100

    add empty_label

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -4,17 +4,21 @@ from django.core.exceptions import ValidationError
 
 
 class AddReviewForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['type'].empty_label = "Select type"
+        self.fields['category'].empty_label = "Select category"
+        self.fields['tags'].empty_label = "Select tags"
+
+
     class Meta:
         model = Review
         fields = ['title', 'text', 'images', 'type', 'category', 'tags', 'is_published']
         widgets = {
             'title': forms.TextInput(attrs={'placeholder': 'Title'}),
             'text': forms.Textarea(attrs={'placeholder': 'Text'}),
-            'images': forms.FileInput(attrs={'placeholder': ''}),
-            'type': forms.Select(attrs={'placeholder': 'Select type'}),
-            'category': forms.Select(attrs={'placeholder': 'Select category'}),
-            'tags': forms.Select(attrs={}),
-            'is_published': forms.CheckboxInput(attrs={'class': ''}),
+            'images': forms.FileInput(),
         }
 
     def clean_title(self):
@@ -28,4 +32,3 @@ class AddReviewForm(forms.ModelForm):
         if len(text) < 20:
             raise ValidationError("Your text can't be less than 20 symbols")
         return text
-
