05e34a8d725981c606f4a824ae74257ce2588bb3
Andrey <andreeey.gg@gmail.com>
2024-03-11 12:41:14 +0100

    delete admin.py in app/users and move the code to admin.py in app/blog

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .models import TypeReview, Category, TagReview, Review
+from users.models import User
 
 
+admin.site.register(User, UserAdmin)
 
 class ReviewTypeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
